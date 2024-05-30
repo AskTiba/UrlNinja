6dcee05d940f7ea76c9476b6697dc1d1d1d11705
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-05-31 00:40:38 +0300

    Update Header.tsx

--- UrlNinja/src/components/Header.tsx
+++ UrlNinja/src/components/Header.tsx
@@ -1,13 +1,20 @@
 import { Button } from "../../@/components/ui/button";
+import logo from "../assets/images/logo.svg";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   return (
     <>
-      <main className="mt-6 sticky top-6 text-[very-darkviolet] text-[15px] font-[Poppins] font-[500]">
+      <main className="mt-6 sticky top-6 text-violet-500 text-[15px] mx-[8%] font-[500]">
         <div className="flex justify-between">
           <div className="flex items-center">
-            <div className="mr-10 font-[700] text-[24px]">Shortly</div>
-            <nav className="">
+            <div className="flex gap-80 mr-10">
+              <img src={logo} alt="" className="mobile:w-[65dvw]" />
+              <button className="hidden mobile:block">
+                <MenuIcon fontSize="large" htmlColor="gray" />
+              </button>
+            </div>
+            <nav className="text-gray-300 font-[500] mobile:hidden">
               <ul className="flex gap-5">
                 <li className="">Features</li>
                 <li className="">Pricing</li>
@@ -15,10 +22,10 @@ const Header = () => {
               </ul>
             </nav>
           </div>
-          <div className="flex gap-5">
-            <Button className="">Login</Button>
-            <Button className="bg-[cyan] text-[gray] rounded-[20px] px-5">
-              SignUp
+          <div className="flex gap-5 mobile:hidden">
+            <Button className="text-blue">Login</Button>
+            <Button className="bg-cyan font-[700] text-[#f7f7fa] rounded-[20px] px-5">
+              Sign Up
             </Button>
           </div>
         </div>
