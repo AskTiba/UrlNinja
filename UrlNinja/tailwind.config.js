f0bd7d8c177d4f6727ad551b87fe36c72ed91cde
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-05-31 00:40:12 +0300

    Update tailwind.config.js

--- UrlNinja/tailwind.config.js
+++ UrlNinja/tailwind.config.js
@@ -8,78 +8,34 @@ export const content = [
 ];
 export const prefix = "";
 export const theme = {
-  container: {
-    center: true,
-    padding: "2rem",
+  container: {},
+  extend: {
     screens: {
-      "2xl": "1400px",
+      mobile: { max: "768px" },
+      desktop: "1440px",
     },
     colors: {
       cyan: "hsl(180, 66%, 49%)",
-      "dark-violet": "hsl(257, 27%, 26%)",
-      "very-darkviolet": "hsl(260, 8%, 14%)",
-      "grayish-violet": "hsl(hsl(257, 7%, 63%)",
-      metal: "#565584",
-      tahiti: "#3ab7bf",
-      silver: "#ecebff",
-      "bubble-gum": "#ff77e9",
-      bermuda: "#78dcca",
-    },
-  },
-  extend: {
-    colors: {
-      border: "hsl(var(--border))",
-      input: "hsl(var(--input))",
-      ring: "hsl(var(--ring))",
-      background: "hsl(var(--background))",
-      foreground: "hsl(var(--foreground))",
-      primary: {
-        DEFAULT: "hsl(var(--primary))",
-        foreground: "hsl(var(--primary-foreground))",
+      red: "hsl(0, 87%, 67%)",
+      blue: "hsl(255, 11%, 22%)",
+      gray: {
+        100: "hsl(0, 0%, 75%)",
+        300: "hsl(257, 7%, 63%)",
       },
-      secondary: {
-        DEFAULT: "hsl(var(--secondary))",
-        foreground: "hsl(var(--secondary-foreground))",
-      },
-      destructive: {
-        DEFAULT: "hsl(var(--destructive))",
-        foreground: "hsl(var(--destructive-foreground))",
-      },
-      muted: {
-        DEFAULT: "hsl(var(--muted))",
-        foreground: "hsl(var(--muted-foreground))",
-      },
-      accent: {
-        DEFAULT: "hsl(var(--accent))",
-        foreground: "hsl(var(--accent-foreground))",
-      },
-      popover: {
-        DEFAULT: "hsl(var(--popover))",
-        foreground: "hsl(var(--popover-foreground))",
-      },
-      card: {
-        DEFAULT: "hsl(var(--card))",
-        foreground: "hsl(var(--card-foreground))",
+      violet: {
+        300: "hsl(257, 27%, 26%)",
+        500: "hsl(260, 8%, 14%)",
       },
     },
-    borderRadius: {
-      lg: "var(--radius)",
-      md: "calc(var(--radius) - 2px)",
-      sm: "calc(var(--radius) - 4px)",
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
     },
-    keyframes: {
-      "accordion-down": {
-        from: { height: "0" },
-        to: { height: "var(--radix-accordion-content-height)" },
-      },
-      "accordion-up": {
-        from: { height: "var(--radix-accordion-content-height)" },
-        to: { height: "0" },
-      },
+    fontSize: {
+      base: "1rem",
     },
-    animation: {
-      "accordion-down": "accordion-down 0.2s ease-out",
-      "accordion-up": "accordion-up 0.2s ease-out",
+    fontWeight: {
+      medium: "500",
+      bold: "700",
     },
   },
 };
