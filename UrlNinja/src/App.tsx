90b57613408913cc6ab2aef7c4efe144b5581418
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-05-31 00:39:49 +0300

    Update App.tsx

--- UrlNinja/src/App.tsx
+++ UrlNinja/src/App.tsx
@@ -1,13 +1,29 @@
+import Boost from "./components/Boost";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import LinkInput from "./components/LinkInput";
+import Statistics from "./components/Statistics";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Hero />
-      <LinkInput />
+      <main className="bg-[#f7f7fa] overflow-x-hidden">
+        <div className=" ">
+          <Header />
+          <Hero />
+          <LinkInput />
+          <Statistics />
+          <Cards />
+          <div className="">
+            <Boost />
+          </div>
+          <div className="w-screen">
+            <Footer />
+          </div>
+        </div>
+      </main>
     </>
   );
 };
