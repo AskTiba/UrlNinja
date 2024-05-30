26cd9612a657e59304dd045f71929cd703442565
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-05-31 00:40:24 +0300

    Create Statistics.tsx

---
+++ UrlNinja/src/components/Statistics.tsx
@@ -0,0 +1,13 @@
+const Statistics = () => {
+  return (
+    <main className="flex flex-col justify-center items-center my-20 mx-[8%]">
+      <div className="text-2xl font-[700]">Advanced Statistics</div>
+      <div className="my-2 text-center text-[#a6acb2]">
+        Track how your links are performing across the web with <br /> our
+        advanced statistics dashboard.
+      </div>
+    </main>
+  );
+};
+
+export default Statistics;
