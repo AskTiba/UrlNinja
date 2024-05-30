7705f7f7507c076999f3465051f5212692fcb58c
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-05-31 00:40:34 +0300

    Update Hero.tsx

--- UrlNinja/src/components/Hero.tsx
+++ UrlNinja/src/components/Hero.tsx
@@ -3,21 +3,21 @@ import working from "../assets/images/illustration-working.svg";
 
 const Hero = () => {
   return (
-    <main className="flex mt-10">
+    <main className="flex mt-10 mx-[8%] mobile:flex-col-reverse">
       <div className="w-[60%]">
-        <div className="text-[64px] font-[700] leading-[1.15]">
+        <div className="text-[64px] text-blue font-[700] leading-[1.15] ">
           More than just <br /> shorter links
         </div>
-        <div className="mt-1">
+        <div className="mt-1 text-gray-300 leading-7">
           Build your brand’s recognition and get detailed <br /> insights on how
           your links are performing.
         </div>
-        <Button className="bg-[cyan] text-[gray] rounded-[20px] px-5 py-1 mt-5">
+        <Button className="bg-cyan text-[#f7f7fa] font-[700] rounded-[28px] px-8 py-3 mt-5">
           Get Started
         </Button>
       </div>
-      <div className="">
-        <img className="ml-[35%] w-full h-full" src={working} alt="" />
+      <div className="mobile:w-full ">
+        <img className="ml-[35%] mobile:ml-0 w-[400%] h-full" src={working} alt="" />
       </div>
     </main>
   );
